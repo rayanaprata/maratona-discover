ae99430825a2da2b84e9715905b72270cf5d4f6e
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-04-16 19:43:57 -0300

    corrigindo bugs

--- src/routes.js
+++ src/routes.js
@@ -21,7 +21,7 @@ const profile = {
 routes.get('/', (req, res) => res.render(views + "index"))
 routes.get('/job', (req, res) => res.render(views + "job"))
 routes.post('/job', (req, res) => {
-    consol.log(req.body)
+    console.log(req.body)
 })
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
 routes.get('/profile', (req, res) => res.render(views + "profile", { profile: profile}))
